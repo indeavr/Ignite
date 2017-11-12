07aedb97f989f1329cb595ede0f5017e463ffd3a
Dimitar Gochev <dimitarggochev@gmail.com>
2017-11-12 14:51:07 +0200

    Users View the currently assigned Courses; Click through the slides as logged User; Create My Courses Page

---
+++ Ignite/Ignite.Tests/Controllers/UserCourseControllerTests/Mocks/MockedUserCourseController.cs
@@ -0,0 +1,24 @@
+﻿using Ignite.Controllers;
+using Ignite.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignite.Tests.Controllers.UserCourseControllerTests.Mocks
+{
+    public class MockedUserCourseController : UserCourseController
+    {
+        public MockedUserCourseController(IUserCourseService userCourseService, ApplicationUserManager userManager)
+            :base(userCourseService,userManager)
+        {
+
+        }
+
+        public new void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ignite/Ignite/Scripts/MyCourses.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $("li.state-tabs").on("click", function () {
+   
+        $("li.state-tabs").removeClass("active");
+        $(this).addClass("active");
+    })
+})
